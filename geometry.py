f069dc90abc658e71fa8c5c5793265f770a6ece2
ecker <textmodelview@gmail.com>
2021-12-09 22:17:06 +0100

    init

---
+++ geometry.py
@@ -0,0 +1,446 @@
+# -*- coding:latin-1 -*-
+
+"""
+Variante der affinen Geometry, die sich in wx integriert. Es werden 
+Point, Rect und Size von wx verwendet. Es ist zu beachten, dass
+Koordinaten immer aus ganzen Zahlen bestehen.
+
+Der Trafo kann auch mit Float-Koordinaten umgehehn, wenn sie als Tupel
+übegeben werden: trafo.TransformPoint((1.3, 2.1))
+"""
+
+import math
+import wx
+
+from math import sqrt
+from copy import copy
+
+from wx import Point, Rect
+
+
+# Methoden für Size
+def grown(size, dx, dy):
+    r = wx.Size(*size)
+    r.IncBy(dx, dy)
+    return r    
+wx.Size.Grown = grown
+
+# Methoden für Rect
+def center(rect):
+    position = rect.Position
+    w, h = rect.Size
+    return position+(w/2, h/2)
+wx.Rect.Center = center
+
+def moved(rect, point):
+    r = wx.Rect(*rect)
+    r.Position += wx.Point(*point)
+    return r    
+wx.Rect.Moved = moved
+
+def grown(rect, dx, dy):
+    r = wx.Rect(*rect)
+    r.Inflate(dx, dy)
+    return r    
+wx.Rect.Grown = grown
+
+def contains_any(rect, point_or_rect):
+    if isinstance(point_or_rect, wx.Rect):
+        return rect.ContainsRect(point_or_rect)
+    return rect.ContainsXY(*point_or_rect)        
+wx.Rect.__contains__ = contains_any
+
+def area(rect):
+    w, h = rect.Size
+    return w*h
+wx.Rect.Area = property(area)
+
+def _ge(rect, other):
+    return rect.Area >= other.Area
+wx.Rect.__ge__ = _ge
+    
+def _gt(self, other):
+    return self.Area > other.Area
+wx.Rect.__gt__ = _gt
+    
+def _le(self, other):
+    return self.Area <= other.Area
+wx.Rect.__le__ = _le
+    
+def _lt(self, other):
+    return self.Area < other.Area
+wx.Rect.__lt__ = _lt
+        
+
+
+# Methoden für Point
+# ACHTUNG:
+#
+# wx.Point is immutable. "point += xxx" kann zu abstürzen führen
+def multiplied(point, f):
+    x, y = point
+    return wx.Point(f*x, f*y)
+wx.Point.__mul__ = multiplied
+wx.Size.__mul__ = multiplied
+wx.Point.__rmul__ = multiplied
+wx.Size.__rmul__ = multiplied
+
+def divided(point, f):
+    x, y = point
+    return wx.Point(x/f, y/f)
+wx.Point.__div__ = divided
+wx.Size.__div__ = divided
+wx.Point.__rdiv__ = divided
+wx.Size.__rdiv__ = divided
+
+def negated(point):
+    x, y = point
+    return wx.Point(-x, -y)
+wx.Point.__neg__ = negated
+
+def length(point):
+    x, y = point
+    return math.sqrt(x*x+y*y)
+wx.Point.Length = length   
+
+def polar(point):
+    x,y = point
+    return math.hypot(x, y), math.atan2(x,y)
+wx.Point.Polar = polar
+
+def normalized(point):
+    x,y = point
+    l = math.hypot(x, y)
+    return wx.Point(x/l, y/l)
+wx.Point.normalized = normalized
+
+def transformed(point, trafo):
+    return trafo.TransformPoint(point)
+wx.Point.Transformed = transformed    
+        
+def rounded(point):
+    x, y = point
+    return wx.Point(round(x), round(y))
+wx.Point.Rounded = rounded
+
+
+def xxdist_point_line(p, a, b):
+    """Berechnet den Abstand zwischen dem Punkt p und dem Segment ab
+    
+       p, a und b brauchen keine Punkte zu sein
+    """
+    x1, y1 = a
+    x2, y2 = b
+    x3, y3 = p
+    u = ((x3-x1)*(x2-x1)+(y3-y1)*(y2-y1))/sqrt((x2-x1)**2+(y2-y1)**2)
+    
+    x = x1 + u*(x2-x1)
+    y = y1 + u*(y2-y1)     
+    
+    return sqrt((x-x3)**2+(y-y3)**2)
+    
+
+def dist_point_line(c, a, b):
+    cx, cy = map(float, c)
+    bx, by = map(float, b)
+    ax, ay = map(float, a)
+
+    """
+    Let the point be C (Cx,Cy) and the line be AB (Ax,Ay) to (Bx,By).
+    Let P be the point of perpendicular projection of C on AB.  The parameter
+    r, which indicates P's position along AB, is computed by the dot product 
+    of AC and AB divided by the square of the length of AB:
+    
+    (1)     AC dot AB
+        r = ---------  
+            ||AB||^2
+    
+    r has the following meaning:
+    
+        r=0      P = A
+        r=1      P = B
+        r<0      P is on the backward extension of AB
+        r>1      P is on the forward extension of AB
+        0<r<1    P is interior to AB
+    
+    The length of a line segment in d dimensions, AB is computed by:
+    
+        L = sqrt( (Bx-Ax)^2 + (By-Ay)^2 + ... + (Bd-Ad)^2)
+
+    so in 2D:   
+    
+        L = sqrt( (Bx-Ax)^2 + (By-Ay)^2 )
+    
+    and the dot product of two vectors in d dimensions, U dot V is computed:
+    
+        D = (Ux * Vx) + (Uy * Vy) + ... + (Ud * Vd)
+    
+    so in 2D:   
+    
+        D = (Ux * Vx) + (Uy * Vy) 
+    
+    So (1) expands to:
+    
+            (Cx-Ax)(Bx-Ax) + (Cy-Ay)(By-Ay)
+        r = -------------------------------
+                          L^2
+
+    The point P can then be found:
+
+        Px = Ax + r(Bx-Ax)
+        Py = Ay + r(By-Ay)
+
+    And the distance from A to P = r*L.
+
+    Use another parameter s to indicate the location along PC, with the 
+    following meaning:
+           s<0      C is left of AB
+           s>0      C is right of AB
+           s=0      C is on AB
+
+    Compute s as follows:
+
+            (Ay-Cy)(Bx-Ax)-(Ax-Cx)(By-Ay)
+        s = -----------------------------
+                        L^2
+
+
+    Then the distance from C to P = |s|*L.
+
+    """
+
+    r_numerator = (cx-ax)*(bx-ax) + (cy-ay)*(by-ay)
+    r_denomenator = (bx-ax)*(bx-ax) + (by-ay)*(by-ay)
+    r = r_numerator / r_denomenator
+
+    px = ax + r*(bx-ax)
+    py = ay + r*(by-ay)
+
+    s =  ((ay-cy)*(bx-ax)-(ax-cx)*(by-ay) ) / r_denomenator
+
+    distanceLine = abs(s)*sqrt(r_denomenator)
+
+    #
+    # (xx,yy) is the point on the lineSegment closest to (cx,cy)
+    #
+    xx = px
+    yy = py
+
+    if (r >= 0) and (r <= 1):
+        distanceSegment = distanceLine
+    else:
+        dist1 = (cx-ax)*(cx-ax) + (cy-ay)*(cy-ay)
+        dist2 = (cx-bx)*(cx-bx) + (cy-by)*(cy-by)
+        if (dist1 < dist2):
+            xx = ax
+            yy = ay
+            distanceSegment = sqrt(dist1)
+        else:
+            xx = bx
+            yy = by
+            distanceSegment = sqrt(dist2)
+    return distanceSegment
+
+        
+class _PointArray:
+
+    _data = None # eine Liste von Points
+    def __init__(self, data=None):
+        if data is not None:
+            if isinstance(data, PointArray):
+                self._data = data._data
+            else:
+                d = []
+                d.extend(data)
+                self._data = d
+        else:
+            self._data = []
+        
+    def __delitem__(self, i):
+        del self._data[i]
+        
+    def __getitem__(self, i):
+        return self._data[i]
+        
+    def __len__(self):
+        return len(self._data)
+        
+    def Append(self, p):
+        self.Insert(len(self._data), p)
+        
+    def Transformed(self, trafo):
+        r = []
+        tranformed = trafo.TransformPoint
+        for point in self._data:
+            r.append(transformed(point))
+        return PointArray(data=r)
+        
+    def GetBounds(self):
+        data = self._data
+        if not len(data):
+            return wx.Rect()
+            
+        point = data[0]
+        xmin = xmax = point[0]
+        ymin = ymax = point[1]
+        
+        for point in data[1:]:
+            xmin = min(xmin, point[0])
+            xmax = max(xmax, point[0])
+            ymin = min(ymin, point[1])
+            ymax = max(ymax, point[1])
+            
+        return wx.Rect(xmin, ymin, xmax, ymax)
+    
+class FloatPointArray(_PointArray):
+    """
+    Die Punkte werden nicht als Integer, sondern als Float abgespeichert.
+    """
+    def Insert(self, i, point):        
+        x, y = point
+        self._data.insert(float(x), float(y))
+
+    def Transformed(self, trafo):
+        r = []
+        tranformed = trafo.TransformPointFloat
+        for point in self._data:
+            r.append(transformed(point))
+        return PointArray(data=r)
+        
+    def Rounded(self):
+        data = [wx.Point(round(x), round(y)) for x, y in self._data]
+        return IntPointArray(data)
+        
+        
+        
+class IntPointArray(_PointArray):
+    def Insert(self, i, point):
+        self._data.insert(i, wx.Point(*point))
+        
+PointArray = IntPointArray
+
+        
+class SingularMatrix(Exception):
+    pass
+
+class Trafo(object):
+    def __init__(self, m11, m12, m21, m22, v1, v2):
+        self.m11 = float(m11)
+        self.m12 = float(m12)
+        self.m21 = float(m21)
+        self.m22 = float(m22)
+        self.v1 = float(v1)
+        self.v2 = float(v2)
+        
+    def __repr__(self):
+        return "Trafo(%.10g, %.10g, %.10g, %.10g, %.10g, %.10g)" % \
+               (self.m11, self.m21, self.m12, self.m22, self.v1, self.v2)
+
+    def __cmp__(self, t):
+        if self.m11 == t.m11 and self.m12 == t.m12 and \
+           self.m21 == t.m21 and self.m22 == t.m22 and \
+           self.v1 == t.v1 and self.v2 == t.v2:
+            return 1
+        return cmp(id(self), id(t))
+    
+    def __call__(self, *args):
+        if len(args) == 1:
+            p = args[0]
+            return p.Transformed(self)
+
+    def TransformPoint(self, p):
+        x, y = p
+        return wx.Point(self.m11 * x + self.m12 * y + self.v1, \
+                        self.m21 * x + self.m22 * y + self.v2)
+                     
+    def TransformPointFloat(self, p):
+        x, y = p
+        return (self.m11 * x + self.m12 * y + self.v1, \
+                self.m21 * x + self.m22 * y + self.v2)
+                     
+    def TransformRect(self, r):
+        return wx.Rect(
+            self.transform_xy(r.Left, r.Bottom), 
+            self.transform_xy(r.Right, r.Top))
+
+    def TransformTrafo(self, t):
+        return Trafo(self.m11*t.m11 + self.m12*t.m21,
+                     self.m21*t.m11 + self.m22*t.m21,
+                     self.m11*t.m12 + self.m12*t.m22,
+                     self.m21*t.m12 + self.m22*t.m22,
+                     self.m11*t.v1 + self.m12*t.v2 +self.v1,
+                     self.m21*t.v1 + self.m22*t.v2 +self.v2)
+
+    def Transformed(self, trafo):
+        return trafo.TransformTrafo(self)     
+                     
+    def Inverse(self):
+        det = self.m11 * self.m22 - self.m12 * self.m21
+
+        if det == 0:
+            raise SingularMatrix("inverting singular matrix")
+        
+        m11 = self.m22 / det;
+        m12 = -self.m12 / det;
+        m21 = -self.m21 / det;
+        m22 = self.m11 / det;
+
+        return Trafo(m11, m21, m12, m22,
+                     -m11 * self.v1 - m12 * self.v2,
+                     -m21 * self.v1 - m22 * self.v2);
+                     
+          
+class Identity(Trafo):
+    def __init__(self):
+        Trafo.__init__(self, 1.0, 0, 0, 1.0, 0, 0)
+
+IDENTITY = Identity()
+
+
+class Stretch(Trafo):
+    def __init__(self, f1, f2=None):
+        if f2 is None:
+            f2 = f1
+        Trafo.__init__(self, float(f1), 0, 0, float(f2), 0, 0)
+        
+
+class Translation(Trafo):
+    def __init__(self, p):
+        Trafo.__init__(self, 1., 0,0,1., p[0], p[1])
+
+class Rotation(Trafo):
+    def __init__(self, angle, center = (0,0)):
+        s = math.sin(angle)
+        c = math.cos(angle)
+        cx, cy = center
+        
+        # compute offset.
+        # The rotation around center is
+        # T(p) = center + M * (p - center)
+        # => offset = center - M * center
+     
+        offx = cx - c * cx + s * cy
+        offy = cy - s * cx - c * cy
+
+        Trafo.__init__(self, c, s, -s, c, offx, offy);
+    
+if __name__ == '__main__':
+    p = wx.Point(1, 2)
+    print 2*p
+    print p*2
+    print p/2
+    print p/2.0
+    
+    
+    trafo = Translation((2, 5))
+    print trafo(p)
+    
+    inv = trafo.Inverse()
+    print inv(trafo(p))
+
+    size = wx.Size(10, 10)
+    size2 = size.Grown(3, 4)
+    w, h = size2
+    print w, h
+    
+    print wx.Rect(10, 10, 40, 30).Center()
---
+++ view.py
@@ -0,0 +1,193 @@
+
+
+import sys
+import wx
+import math
+from math import sin, cos, pi, sqrt
+
+def overridable_property(name, doc = None):
+    setter_name = intern('set_' + name)
+    return property(
+        lambda self: getattr(self, '_'+name),
+        lambda self, value: getattr(self, setter_name)(value),
+        None,
+        doc)
+
+
+def dist(p1, p2):
+    return sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)
+    
+    
+class Canvas(wx.Window):
+    zoom = overridable_property('zoom')
+    shift = overridable_property('shift')
+    p1 = overridable_property('p1')
+    p2 = overridable_property('p2')
+    transient = overridable_property('transient')
+    _zoom = 1.0
+    _shift = 0.0, 0.0
+    _p1 = (0, 50)
+    _p2 = (100, 50)
+    _transient = None
+    bmp = None
+    def __init__(self, *args, **kwds):
+        wx.Window.__init__(self, *args, **kwds)
+        
+        self.SetBackgroundColour('white')
+        self.Bind(wx.EVT_PAINT, self.on_paint)
+        self.Bind(wx.EVT_ERASE_BACKGROUND, lambda e: None)
+        
+        self.Bind(wx.EVT_MOUSE_EVENTS, self.mouse_event)
+        #self.Bind(wx.EVT_MOUSE_CAPTURE_LOST, self.capturelost_event)
+        
+        self.SetFocus()
+
+    def on_paint(self, event):
+        buffer = wx.EmptyBitmap(*self.Size)
+        dc = wx.BufferedPaintDC(self, buffer)
+        gc = wx.GraphicsContext.Create(dc)
+        #gc.SetPen(wx.WHITE_PEN)
+        #gc.PushState()
+        w, h = self.Size
+        
+        shift = self._shift
+        gc.Translate(-shift[0], -shift[1])
+        
+        p1 = self._p1
+        p2 = self._p2
+        alpha = math.atan2((p1[1]-p2[1]), p2[0]-p1[0])
+        print "paint alpha=", alpha
+        gc.Rotate(alpha)
+        
+        zoom = self._zoom
+        gc.Scale(zoom, zoom)
+        
+        self._trafo =  gc.GetTransform()
+        gc.SetPen(wx.RED_PEN)
+        #gc.SetBrush(wx.YELLOW_BRUSH)
+
+        bmp = self.bmp
+        if bmp:
+            gc.DrawBitmap(bmp, -0, -0, bmp.Width, bmp.Height)
+            
+        s = 10.0/zoom
+        pen = wx.Pen(colour="red", width=2.0/zoom)
+        gc.SetPen(pen)
+        gc.DrawEllipse(p1[0], p1[1], s, s)
+        gc.DrawEllipse(p2[0], p2[1], s, s)
+        
+        p = self._transient
+        if p:
+            pen = wx.Pen(colour="grey", width=2.0/zoom)
+            gc.SetPen(pen)
+            gc.DrawEllipse(p[0], p[1], s, s)
+            
+        #gc.PopState()
+        
+    def load_image(self, imagefile):
+        bmp = wx.EmptyBitmap(1, 1)
+        bmp.LoadFile(imagefile, wx.BITMAP_TYPE_ANY) 
+        self.bmp = bmp
+        
+    def set_zoom(self, zoom):
+        if zoom != self._zoom:
+            self._zoom = zoom
+            self.Refresh()
+
+    def set_shift(self, shift):
+        if shift != self._shift:
+            self._shift = shift
+            self.Refresh()
+            
+    def set_p1(self, p1):
+        if p1 != self._p1:
+            self._p1 = p1
+            self.Refresh()
+
+    def set_p2(self, p2):
+        if p2 != self._p2:
+            self._p2 = p2
+            self.Refresh()
+
+    def set_transient(self, p):
+        if p != self._transient:
+            self._transient = p
+            self.Refresh()
+
+    def get_trafo(self):
+        p1 = self._p1
+        p2 = self._p2
+        alpha = math.atan2((p1[1]-p2[1]), p2[0]-p1[0])
+        from geometry import Translation, Rotation, Stretch
+        trafo = Translation(-wx.Point(*self._shift))
+        trafo = Rotation(-alpha)(trafo)
+        trafo = Stretch(self._zoom)(trafo)
+        print "alpha=", alpha
+        return trafo
+            
+    _dragstart = None # in screen coordinates
+    def mouse_event(self, event):
+        trafo = self.get_trafo()        
+        p1 = wx.Point(*self._p1)
+        p2 = wx.Point(*self._p2)
+        p1_ = trafo(p1)
+        p2_ = trafo(p2)
+        
+        inv = trafo.Inverse()
+        print "trafo=", trafo
+        print "inv=", inv
+
+        print "p screen=", event.Position
+        print "p bitmap=", trafo(event.Position)
+        print "p test=", inv(event.Position)
+
+        if event.Moving():
+            p = event.Position
+            if dist(p1_, event.Position)<10:
+                self._current = 1
+                self.SetCursor(wx.StockCursor(wx.CURSOR_CROSS))
+            elif dist(p2_, event.Position)<10:
+                self._current = 2
+                self.SetCursor(wx.StockCursor(wx.CURSOR_CROSS))
+            else:
+                self._current = None
+                self.SetCursor(wx.StockCursor(wx.CURSOR_DEFAULT))
+        if event.Dragging() and self._current:
+            #print "current=", self._current
+            inv = trafo.Inverse()
+            p = inv.TransformPoint(event.Position)
+            if self._dragstart is None:                
+                self._dragstart = event.Position
+            else:
+                self.Refresh()
+            points = None, p1_, p2_
+            self._transient = inv(points[self._current]+event.Position-self._dragstart)
+            self.Refresh()
+        elif event.ButtonUp():        
+            print "release"
+            
+            if self._transient is not None:
+                if self._current == 1:
+                    self.p1 = self._transient
+                elif self._current == 2:
+                    self.p2 = self._transient
+            
+            self._transient = None
+            self._dragstart = None
+            self._current = None
+            
+
+if __name__ == '__main__':
+    app = wx.App()
+    f = wx.Frame(None)
+    canvas = Canvas(f)
+    canvas.load_image("ah79k135.gif")
+    f.Show()
+
+
+    canvas.transient = (50, 50)
+    #canvas.zoom = 2
+    canvas.p1 = 30, 40
+    app.MainLoop()
+
+
