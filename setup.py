bb65cf4493c3b07d8e68d957d5c2d283bc542526
ecker <textmodelview@gmail.com>
2021-12-29 23:04:14 +0100

    Dumy files for setup and README

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: latin-1 -*-
+
+
+from setuptools import setup, find_packages
+
+with open('README') as file:
+    long_description = file.read()
+
+setup(name='whichfoil',
+      version='0.0',
+      description = 'A tool for identifying airfoils from bitmap images.',
+      long_description = long_description,
+      author='C. Ecker',
+      author_email='textmodelview@gmail.com',
+      #url='https://pypi.python.org/',
+      license='BSD',
+      packages=find_packages(exclude=['etc', 'test']),
+      entry_points={
+          'console_scripts': [
+              'whichfoil=whichfoil.main:main',
+          ],
+      },
+      platforms = ['any'],
+     )
+
